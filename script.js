480e095915b93af0adfd809d397355476701e8b8
Pooja Manoj <poojainsaipooja@gmail.com>
2020-12-07 19:59:09 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,13 @@
+function bclick(val)
+{
+    document.getElementById("screen").value+=val
+}
+function clears(){
+     document.getElementById("screen").value=""
+}
+function equalc(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+    
+}
